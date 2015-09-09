cea7067a1c10efaecdd96a538611f9a723cc0527
Jackwitwicky <jacknkiarie@gmail.com>
2015-09-09 17:21:53 +0300

    Done adding some basic styling

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -10,7 +10,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
@@ -18,7 +18,7 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
       flash[:success] = "Your pin has been created"
